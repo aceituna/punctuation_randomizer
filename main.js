0683ed62378b6a68cea7f36ad78327d51b470695
nostoc@bk.ru <nostoc@bk.ru>
2018-01-24 20:34:39 +0300

    comment out unused

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const allPunctuationMarks =  ['.', ',', '!', ':', ';', '/?',
-//'...' //needs some consideration
+const allPunctuationMarks =  ['.', ',', '!', ':', ';',
+ '/?', '...' //need some consideration
  ];
 
-let punctuationMarks = ['.', ',', '!', ':', ';', '/?' ];
+let punctuationMarks = ['.', ',', '!', ':', ';' ];
 
 
 run()
 
 function run(){
-    setPunctuationMarksText();
+//    setPunctuationMarksText();
     clearOutput();
 }
 
@@ -34,7 +34,7 @@ function getRandomMark(){
     return punctuationMarks[Math.floor(Math.random() * punctuationMarks.length)];
 }
 
-function setPunctuationMarksText(){
+//function setPunctuationMarksText(){
 //    let punctuationMarksCheckboxList = document.getElementById("punctuation_marks_list");
 //    for (let mark of allPunctuationMarks){
 //        let newMark = document.createElement("div");
@@ -48,14 +48,14 @@ function setPunctuationMarksText(){
 //
 //        punctuationMarksCheckboxList.appendChild(newMark);
 //        setCheckboxValue(mark);
-    }
+//    }
 
 
 
 
 //    document.getElementById('punctuation_marks_list').innerText = punctuationMarks.join('');
 
-}
+//}
 //function getChecked(){
 //    let punctuationMarksCheckboxes = document.getElementByClassName('mark-checkbox');
 //    let marksToUseList = [];
@@ -65,10 +65,10 @@ function setPunctuationMarksText(){
 //        }
 //    }
 //}
-function setCheckboxValue(identifier){
-    let checkboxInput = document.getElementById(identifier);
-    checkboxInput.checked = punctuationMarks.includes(identifier);
-}
+//function setCheckboxValue(identifier){
+//    let checkboxInput = document.getElementById(identifier);
+//    checkboxInput.checked = punctuationMarks.includes(identifier);
+//}
 function clearOutput(){
     setOutput('');
 }
